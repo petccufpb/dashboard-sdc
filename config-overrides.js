f073da6d536bbcfe92b20cebfb61f1dd4dab7432
Matheus Lima <matheus.lima@lavid.ufpb.br>
2018-11-13 17:36:23 -0300

    fix: lint

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,6 @@
 const { injectBabelPlugin } = require('react-app-rewired');
 
-module.exports = function override(config, env) {
+module.exports = function override(config) {
 	// do stuff with the webpack config...
 	config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }], config);
 	return config;
